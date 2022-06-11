cb7d1d46e8c055660d541a9237bcefc3c7f667ec
zeineddinbachtiar <86919853+zeineddinbachtiar@users.noreply.github.com>
2022-06-11 22:52:11 +0700

    Add files via upload

---
+++ Project-Proglan/prolan-sim/include/mahasiswa.hpp
@@ -0,0 +1,35 @@
+#ifndef __MAHASISWA_HPP__
+#define __MAHASISWA_HPP__
+
+
+#include <iostream>
+#include "include/person.hpp"
+
+class mahasiswa : public person {
+private:
+	std::string nrp;
+	std::string departemen;
+	int tahunmasuk;
+	int semesterke;
+	int skslulus;
+	float ipk;
+	std::vector<float> ips;
+
+public:
+	mahasiswa(std::string id, std::string nama, int dd, int mm, int yy,
+					std::string nrp, std::string departemen, int tahunmasuk);
+
+	void setSemester(int semesterke);
+	int getSemester();
+
+	void setSKSLulus(int skslulus);
+	int getSKSLulus();
+
+	void hitungIPK();
+
+	void setIPS(int semester, float ips);
+	float getIPS(int semester);
+	std::vector<float> getAllIPS();
+};
+
+#endif
